0f34ff416e5df49cfd554a7954798c2841eb8fad
Rick <rick@ricksdomein.nl>
2022-01-09 09:52:43 +0100

    OpenBSD compatible

--- config.mk
+++ config.mk
@@ -29,9 +29,10 @@ STLDFLAGS = $(LIBS) $(LDFLAGS)
 
 # OpenBSD:
 #CPPFLAGS = -DVERSION=\"$(VERSION)\" -D_XOPEN_SOURCE=600 -D_BSD_SOURCE
-#LIBS = -L$(X11LIB) -lm -lX11 -lutil -lXft \
+#LIBS = -L$(X11LIB) -lm -lX11 -lutil -lXft -lXrender \
 #       `$(PKG_CONFIG) --libs fontconfig` \
-#       `$(PKG_CONFIG) --libs freetype2`
+#       `$(PKG_CONFIG) --libs freetype2` \
+#       `$(PKG_CONFIG) --libs harfbuzz`
 
 # compiler and linker
 # CC = c99
